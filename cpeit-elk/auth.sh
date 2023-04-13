3975567f5929f73355b6c960b74257c735f319f5
RaXx00n <110571113+RaXx00n@users.noreply.github.com>
2023-04-13 15:26:36 -0400

    Update auth.sh

--- cpeit-elk/auth.sh
+++ cpeit-elk/auth.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
 # Run the elasticsearch-create-enrollment-token command
-token=$(./elasticsearch-create-enrollment-token --url https://127.0.0.1:9200 -s kibana)
+output=$(./elasticsearch-create-enrollment-token --url https://127.0.0.1:9200 -s kibana)
 
-# Copy the token to the clipboard
-echo $token | pbcopy
+# Extract the token from the output
+token=$(echo $output | grep -oP 'enrollment_token":"\K[^"]+')
 
-# Open Kibana and wait for it to load
-open http://localhost:5601
-echo "Waiting for Kibana to load..."
-sleep 30
+# Open Firefox and navigate to the Kibana endpoint
+firefox https://your-kibana-url
 
-# Get the name of the frontmost browser window
-browser=$(osascript -e 'tell application "System Events" to get name of first process whose frontmost is true')
+# Wait for Kibana to load
+sleep 10
 
-# Enter the token into the browser
-echo "Entering token into $browser..."
-osascript -e 'tell application "System Events" to keystroke "a" using {command down}'
-osascript -e 'tell application "System Events" to keystroke "'"$token"'"'
-osascript -e 'tell application "System Events" to keystroke return'
+# Enter the token into the Kibana UI
+xdotool type "$token"
+xdotool key Return
+
