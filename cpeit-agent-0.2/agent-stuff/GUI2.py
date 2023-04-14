ab5121d0bd1b996f297b767cb84bf2baf3835a81
RaXx00n <110571113+RaXx00n@users.noreply.github.com>
2023-04-14 18:31:08 -0400

    Add files via upload

--- cpeit-agent-0.2/agent-stuff/GUI2.py
+++ cpeit-agent-0.2/agent-stuff/GUI2.py
@@ -1,107 +1,62 @@
 import tkinter as tk
-import os
-import datetime
+from tkinterweb import HtmlFrame
 import subprocess
+import shutil
+import os
 
-def run_script(script_path):
-    os.system(f'powershell.exe -ExecutionPolicy RemoteSigned -File "{script_path}"')
+class CPEitGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("CPEit")
+        master.attributes('-fullscreen', True)
+        master.configure(bg='black')
+        master.grid_rowconfigure(0, weight=1)
+        master.grid_columnconfigure(0, weight=1)
+        master.grid_columnconfigure(1, weight=1)
 
-def combine_json():
-    print("Processing - Combining JSON files")
-    subprocess.run(["python", "combine_json.py"], capture_output=True, text=True)
+        # Create left panel
+        left_panel = tk.Frame(master, bg='black', width=100)
+        left_panel.grid(row=0, column=0, sticky='nsew')
+        left_panel.grid_propagate(True)
+        left_panel.grid_rowconfigure(0, weight=1)
+        left_panel.grid_rowconfigure(1, weight=1)
 
-def convert_json():
-    print("Processing - Combining JSON files")
-    subprocess.run(["python", "json2ndjson.py"], capture_output=True, text=True)
+        # Create button "Build Inventory"
+        build_button = tk.Button(left_panel, text="Build Inventory", command=self.build_inventory, bg='black', fg='white', font=('Helvetica', 14))
+        build_button.grid(row=0, column=0, sticky='nsew')
 
-def cpe_guess():
-    print("Generating potential CPEs...")
-    subprocess.run(["python", "cpe_guess.py"], capture_output=True, text=True)
-    subprocess.run(["python", "cpe_guess2.py"], capture_output=True, text=True)
-    print("Potential CPEs generated.")
+        # Create button "Export CPEs"
+        export_button = tk.Button(left_panel, text="Export CPEs", command=self.export_cpes, bg='black', fg='white', font=('Helvetica', 14))
+        export_button.grid(row=1, column=0, sticky='nsew')
 
-def generate_inventory():
-    print("Generating inventory...")
-    subprocess.run(["python", "generate_inventory.py"], capture_output=True, text=True)
-    subprocess.run(["python", "generate_inventory_with_guess.py"], capture_output=True, text=True)
-    print("Inventory generated.")
+        # Create image above the buttons
+        image = tk.PhotoImage(file="image.gif")
+        img_label = tk.Label(left_panel, image=image, bg='black')
+        img_label.image = image
+        img_label.grid(row=2, column=0, sticky='nsew')
 
-def view_inventory():
-    print("Viewing inventory...")
-    os.startfile("Software-Inventory.html")
+        # Create HTML reader widget
+        self.html_frame = HtmlFrame(master, horizontal_scrollbar='none')
+        self.html_frame.grid(row=0, column=1, sticky='nsew')
 
-def build_inventory():
-    if registry_check.get() == 1:
-        print("Building inventory from Registry Tree (HKLM)...")
-        run_script('Get-Item8.ps1')
-        print("Inventory from Registry Tree (HKLM) built.")
+        # Add weight to the rows and columns to fill remaining space
+        master.grid_rowconfigure(0, weight=3)
+        master.grid_columnconfigure(0, weight=3)
+        master.grid_columnconfigure(1, weight=3)
+        left_panel.grid(sticky='nsew')
 
-    if wmi_check.get() == 1:
-        print("Building inventory from Windows Management Instrumentation...")
-        run_script('Get-WMIobject8.ps1')
-        print("Inventory from Windows Management Instrumentation built.")
+    def build_inventory(self):
+        subprocess.run(['python', 'ENUMERATE.py'])
+        html_path = 'Software-Inventory.html'
+        with open(html_path, 'r') as f:
+            html_content = f.read()
+        self.html_frame.load_html(html_content)
 
-    if package_check.get() == 1:
-        print("Building inventory from PackageManagement...")
-        run_script('Get-Package8.ps1')
-        print("Inventory from PackageManagement built.")
+    def export_cpes(self):
+        shutil.copyfile("PotentialCPE.txt", os.path.join(os.path.expanduser("~"), "Desktop", "PotentialCPE.txt"))
+        os.startfile(os.path.join(os.path.expanduser("~"), "Desktop", "PotentialCPE.txt"))
 
-    combine_json()
-    convert_json()
-    cpe_guess()
-    generate_inventory()
 
-# Create the main window
 root = tk.Tk()
-root.title("Software Inventory Builder")
-
-# Create the left frame for buttons
-left_frame = tk.Frame(root)
-left_frame.pack(side=tk.LEFT, padx=10, pady=10)
-
-# Create the right frame for output
-right_frame = tk.Frame(root)
-right_frame.pack(side=tk.RIGHT, padx=10, pady=10)
-
-# Create the welcome label
-welcome_label = tk.Label(left_frame, text="Welcome to the Software Inventory Builder!")
-welcome_label.pack(pady=10)
-
-# Create the check boxes
-registry_check = tk.IntVar()
-wmi_check = tk.IntVar()
-package_check = tk.IntVar()
-
-registry_checkbox = tk.Checkbutton(left_frame, text="Registry Tree (HKLM)", variable=registry_check)
-wmi_checkbox = tk.Checkbutton(left_frame, text="Windows Management Instrumentation", variable=wmi_check)
-package_checkbox = tk.Checkbutton(left_frame, text="PackageManagement", variable=package_check)
-
-registry_checkbox.pack()
-wmi_checkbox.pack()
-package_checkbox.pack()
-
-# Create the Build Inventory button
-build_button = tk.Button(left_frame, text="Build Inventory", command=build_inventory)
-build_button.pack(pady=10)
-
-# Create the View Inventory button
-view_button = tk.Button(left_frame, text="View Inventory", command=view_inventory)
-view_button.pack(pady=10)
-
-# Create the output text widget
-output_text = tk.Text(right_frame, height=20, width=80)
-output_text.pack()
-
-# Redirect stdout to the output text widget
-import sys
-class StdoutRedirector:
-    def __init__(self, text_widget):
-        self.text_widget = text_widget
-
-    def write(self, string):
-        self.text_widget.insert(tk.END, string)
-
-sys.stdout = StdoutRedirector(output_text)
-
-# Start the main loop
+gui = CPEitGUI(root)
 root.mainloop()
