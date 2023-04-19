42c76d6710506e8f1bc4ab5ecec5f3efa9026306
RaXx00n <110571113+RaXx00n@users.noreply.github.com>
2023-04-19 16:39:54 -0400

    Delete Launch Agent.cmd

--- Launch Agent.cmd
+++
@@ -1,9 +0,0 @@
-pip install json2html
-pip install pandas
-pip install tkinterweb
-pip install json
-pip install nvdlib
-pip install Levenshtein
-pip install requests
-cd cpeit-agent-0.6/agent-stuff
-python GUI3.py
